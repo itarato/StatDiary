7b941e4b656eb5495e9228a0eac1d05feac5d799
Peter Arato <it.arato@gmail.com>
2011-06-06 19:37:03 +0200

    Add Login view xib and controller.

---
+++ Classes/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  StatDiary
+//
+//  Created by Peter Arato on 6/6/11.
+//  Copyright 2011 Pronovix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoginViewController : UIViewController {
+
+}
+
+@end
