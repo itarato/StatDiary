4c8e42cbff0610fdb3d3e8e68dd3a60b23a76fd4
Peter Arato <it.arato@gmail.com>
2011-06-11 00:22:50 +0200

    Add stat detail view.

---
+++ Classes/StatDetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  StatDetailsViewController.h
+//  StatDiary
+//
+//  Created by Peter Arato on 6/10/11.
+//  Copyright 2011 Pronovix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface StatDetailsViewController : UIViewController {
+
+}
+
+@end
